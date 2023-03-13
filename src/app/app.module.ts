4d69f7fb13de33ca65722128803ceed089f4b66b
Thenura <adalanane111@gmail.com>
2023-03-13 07:36:49 +0530

    employee completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VendorComponent } from './vendor/vendor.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VendorComponent
+    VendorComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
